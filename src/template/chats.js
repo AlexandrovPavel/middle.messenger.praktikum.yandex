a9aaf2623d26d8b571af3a2868c112a888c8ba9b
Pavel Alexandrov <alexpavel35@yandex.ru>
2024-05-15 20:01:34 +0300

    Верстка страниц чата

---
+++ src/template/chats.js
@@ -0,0 +1,443 @@
+export const chats = [
+        {
+        id: 1,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 5,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 2,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 11,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 3,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 8,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 4,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 6,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 5,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 4,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 6,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 9,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 7,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 17,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 8,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 29,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 9,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 3,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 10,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 8,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 11,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 10,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 12,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 4,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 13,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 1,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 14,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 9,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 15,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 15,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 16,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 2,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 17,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 5,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 18,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 9,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 19,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 6,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+        {
+        id: 20,
+        title: "my-chat",
+        avatar: "/src/assets/avatar.jpg",
+        unread_count: 1,
+        created_by: 12345,
+        last_message: {
+            user: {
+            first_name: "Ivan",
+            second_name: "Ivanov",
+            avatar: "/assets/avatar.jpg",
+            email: "exempl@email.com",
+            login: "userLogin",
+            phone: "*(***) ***-**-**",
+            },
+            time: "17:15",
+            content:
+            "this is message content this is message content this " +
+            "is message content this is message content this is message " +
+            "content this is message content this is message content",
+        },
+        },
+    ];
+    
